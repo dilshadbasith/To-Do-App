20eb5904f16025e24721fd4a663aff0d7817a167
dilshad <dilshadbasith01@gmail.com>
2021-11-03 12:53:57 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import styles from "./styles.module.css";
+import ListItem from "./components/LisItem";
+
 
 function App() {
+  const [todo, settodo] = useState("")
+  const [todos, settodos] = useState([])
+  console.log(todos);
+  const handleSubmit =() => {
+    settodos([...todos, todo])
+  }
+  const handleDelete = (todo) =>{
+    settodos(todos.filter(item => (item !== todo)))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.contentWrapper}>
+      <div>
+        <div className={styles.title}><h1>To-do-App!</h1></div>
+        <div className={styles.addNew}><h3>Add New To-Do</h3></div>
+        <div className={styles.textBtn}>
+        <div>
+        {todos.map(todo => (
+        <ListItem key={todo} todo={todo} handleDelete={handleDelete}/>
+        ))}
+          <div><input onChange={(e) => settodo(e.target.value)} className={styles.textBox} type="text"></input></div>
+        </div>
+        <div><button onClick={handleSubmit} className={styles.btn}>ADD</button></div>
+        </div>
+      </div>
+      <div className={styles.me}>By Dilshad :)</div>
     </div>
+    
   );
 }
 
---
+++ src/components/LisItem.js
@@ -0,0 +1,12 @@
+import './ListItem.css';
+export default function ListItem(props){
+    const {
+        todo ,handleDelete} = props;
+        return(
+            <div className="listitem">
+                <h5 className="listview">{todo}</h5>
+                <button onClick={()=>handleDelete(todo)}
+                className="delete"><i class="fas fa-trash-alt"></i></button>
+            </div>
+        )
+    }
